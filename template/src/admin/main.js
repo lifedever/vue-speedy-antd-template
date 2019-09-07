8f18a563947f003442fe9f56640a3f81012162e6
gefangshuai <gefangshuai@163.com>
2019-09-07 22:32:52 +0800

    fix bugs

---
+++ template/src/admin/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import Inject from 'vue-speedy-antd/conf/admin-main-inject'
+import pages from 'vue-speedy-antd/src/admin/pages'
+import adminLayout from "vue-speedy-antd/src/layout/admin-layout";
+import modules from './modules'
+import menus from './menu'
+
+import '../assets/override.less'
+
+
+Vue.use(adminLayout, {
+    menus
+})
+
+Vue.use(Inject, {
+    config: {
+        title: 'Vue Speedy Ant Design',
+        shortTitle: 'VD',
+    },
+    pages: pages,
+    modules
+})
--- template/vue.config.js
+++ template/vue.config.js
@@ -1,15 +1,23 @@
 module.exports = require('vue-speedy-antd/conf')({
     lessModifyVars: {   // 用于覆盖主题配色，所有变量如下：https://github.com/vueComponent/ant-design-vue/blob/master/components/style/themes/default.less
-        'primary-color': '#0774a5',
+        'primary-color': '#0682a5',
     },
-    title: 'Vue Speedy Ant Design',     // 站点标题
-    baseUrl: '/',                       // vue cli baseUrl
+    admin: {
+        name: 'boss',
+        entry: 'src/admin/main.js',
+        title: 'Boss 后台管理'
+    },
+    index: {
+        entry: 'src/index/main.js',
+        title: '业务前台'
+    },
+    title: 'Vue Speedy Ant Design',
+    baseUrl: '/',
     devServer: {
-        port: 6161,                     // 前端服务端口
-        // 后端服务请求代理配置
+        port: 6161,
         proxy: {
             '/api': {
-                target: "http://localhost:18080",   // 后端服务地址
+                target: "http://localhost:18080",
                 changeOrigin: true,
                 secure: false,
                 pathRewrite: {
